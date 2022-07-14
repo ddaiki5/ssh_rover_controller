40fbc174c721b97b9b2ca429c19fb8f3ab49b85b
ddaiki5 <62028697+ddaiki5@users.noreply.github.com>
2022-07-15 04:26:58 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/ssh_rover_controller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.ssh_rover_controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
